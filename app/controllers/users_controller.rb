cd9cbb0637ed12ff1513b7762bbf6ed9dd0a3841
yamakawa00 <yamakawa00@pepabo.com>
2017-10-20 15:49:18 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
+
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
   def index
-    # @user = User.new
-    # render 'new'
-    # @users = User.all
-
-    # @users = User.paginate(page: params[:page])
-
     @users = User.where(activated: true).paginate(page: params[:page])
   end
 
@@ -61,6 +57,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
     def user_params
@@ -69,15 +79,6 @@ class UsersController < ApplicationController
     end
 
     # beforeファルター
-
-    # ログイン済みユーザーかどうか確認
-    # def logged_in_user
-    #   unless logged_in?
-    #     store_location
-    #     flash[:danger] = "Please log in."
-    #     redirect_to login_url
-    #   end
-    # end
 
     # 正しいユーザーかどうか確認
     def correct_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # User.create(id: 1, name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
 require 'faker/okinawa'
 
+# ユーザー
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password: "foobar",
@@ -21,10 +22,27 @@ User.create!(name: "Example User",
                activated: true,
                activated_at: Time.zone.now)
 end
+
 # Micropostsを生成
+users = User.order(:created_at).take(1)
+4950.times do
+  content = Faker::Okinawa::Address.city + "出身\n" + \
+  Faker::Okinawa::Address.district + "在住\n" + \
+  Faker::Okinawa::Awamori.name + " と " + Faker::Okinawa::Awamori.name + " が好き"
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
 users = User.order(:created_at).take(6)
 50.times do
   # content = Faker::Lorem.sentence(5)
   content = Faker::Okinawa::Awamori.name + " と " + Faker::Okinawa::Awamori.name + " が好き"
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
